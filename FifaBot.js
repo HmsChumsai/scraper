79536dd8139d1145b6d05defd78d01ac8adf2787
HmsChumsai <ravipong.chumsai@gmail.com>
2014-12-21 04:06:05 +0000

    Succesfull scrape using test.js

---
+++ FifaBot.js
@@ -0,0 +1,2 @@
+var FifaScraper = require('./FifaScraper');
+var test = new FifaScraper('https://www.easports.com/fifa/game-data/stats/thescrewcross/fifa15-ps4/futSeasons');
--- test.js
+++ test.js
@@ -1,31 +1,35 @@
-/*
-Request URL:http://us.soccerway.com/a/block_competition_matches_summary?block_id=page_competition_1_block_competition_matches_summary_6&callback_params=%7B%22page%22%3A%2215%22%2C%22bookmaker_urls%22%3A%5B%5D%2C%22block_service_id%22%3A%22competition_summary_block_competitionmatchessummary%22%2C%22round_id%22%3A%2225191%22%2C%22outgroup%22%3A%22%22%2C%22view%22%3A%221%22%7D&action=changePage&params=%7B%22page%22%3A16%7D
+var phantom = require('node-phantom');
+phantom.create(function(err, ph) {
+  return ph.createPage(function(err, page) {
+    return page.open("https://www.easports.com/fifa/game-data/stats/thescrewcross/fifa15-ps4/futSeasons", function(err, status) {
+      console.log("opened site? ", status);
+      page.includeJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function(err) {
+        //jQuery Loaded.
+        return page.open("https://www.easports.com/fifa/game-data/stats/thescrewcross/fifa15-ps4/futSeasons", function(err, status) {
+          //Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds.
+          setTimeout(function() {
+            return page.evaluate(function() {
+              //Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object.
+              var h2Arr = [],
+                pArr = [];
+              $('div.table-list li div.align-right').each(function() {
+                h2Arr.push($(this).text());
+              });
+              $('p.stat-number').each(function() {
+                pArr.push($(this).text());
+              });
 
-*/
-var obj = {
-        block_id: 'page_competition_1_block_competition_matches_summary_6',
-        callback_params: {
-            "page": 0,
-            "bookmaker_urls": {
-                "13": [{
-                    "link": "http:\/\/www.bet365.com\/home\/?affiliate=365_308136",
-                    "name": "Bet 365"
-                }]
-            },
-            "block_service_id": "competition_summary_block_competitionmatchessummary",
-            "round_id": 25191,
-            "outgroup": false,
-            "view": 2
-        },
-    action: 'changePage',
-    params: {
-        "page": 16
-    }
-};
-
-
-var str = Object.keys(obj).map(function(key) {
-    return encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]);
-}).join('&');
-
-console.log(str);
+              return {
+                h2: h2Arr,
+                p: pArr
+              };
+            }, function(err, result) {
+              console.log(result);
+              ph.exit();
+            });
+          }, 5000);
+        });
+      });
+    });
+  });
+});
